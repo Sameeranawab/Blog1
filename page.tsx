b5073edc8486263f82a40fa2c10bac1d81606cf4
Sameera Nawab <160266378+Sameeranawab@users.noreply.github.com>
2025-01-14 18:55:48 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import Blog from "@/components/Blog/Blog";
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <>
+     <Navbar/>
+     <Blog/>
+    </>
+  );
+}
